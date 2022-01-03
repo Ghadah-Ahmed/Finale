755ca3a2f0eba3b9a57a6fb343308acbb99f336c
Ghadah-Ahmed <moslem.mah@gmail.com>
2022-01-03 14:13:25 +0300

    Deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const cors = require("cors");
-// const POTR = process.env.POTR || 8080;
+const POTR = process.env.POTR || 8080;
 const mongoose = require('mongoose');
 const Menu = require('./router/menu')
 const Users = require('./router/users')
